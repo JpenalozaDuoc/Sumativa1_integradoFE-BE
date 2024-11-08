07035398bfba653d7fd33a16a016ee0ae9b9e2d0
JpenalozaDuoc <163478180+JpenalozaDuoc@users.noreply.github.com>
2024-11-08 10:10:56 -0300

    Delete BE-Sumativa 1/be-grupo15 directory

--- BE-Sumativa 1/be-grupo15/src/main/java/be_grupo15/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package be_grupo15;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Integer> {
-    User findByName(String name);
-
-}
